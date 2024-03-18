e9da2deeab7fbd3718ce1e046bbf8dce94af1d69
aemendez <aemendez@umes.edu.gt>
2024-03-18 09:44:43 -0600

    Agregar archivos de proyecto.

---
+++ Lab_repaso_3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Lab_repaso_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewPropiedades = new System.Windows.Forms.DataGridView();
+            this.dataGridViewPropietarios = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPropiedades)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPropietarios)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewPropiedades
+            // 
+            this.dataGridViewPropiedades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPropiedades.Location = new System.Drawing.Point(30, 26);
+            this.dataGridViewPropiedades.Name = "dataGridViewPropiedades";
+            this.dataGridViewPropiedades.RowHeadersWidth = 51;
+            this.dataGridViewPropiedades.RowTemplate.Height = 24;
+            this.dataGridViewPropiedades.Size = new System.Drawing.Size(346, 259);
+            this.dataGridViewPropiedades.TabIndex = 0;
+            // 
+            // dataGridViewPropietarios
+            // 
+            this.dataGridViewPropietarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPropietarios.Location = new System.Drawing.Point(399, 26);
+            this.dataGridViewPropietarios.Name = "dataGridViewPropietarios";
+            this.dataGridViewPropietarios.RowHeadersWidth = 51;
+            this.dataGridViewPropietarios.RowTemplate.Height = 24;
+            this.dataGridViewPropietarios.Size = new System.Drawing.Size(346, 259);
+            this.dataGridViewPropietarios.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(221, 291);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(346, 259);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(616, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Información";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 558);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dataGridViewPropietarios);
+            this.Controls.Add(this.dataGridViewPropiedades);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPropiedades)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPropietarios)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewPropiedades;
+        private System.Windows.Forms.DataGridView dataGridViewPropietarios;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab_repaso_3/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_repaso_3
+{
+    public partial class Form1 : Form
+    {
+        List<Propiedades> propiedad = new List<Propiedades>();
+        List<Propietarios> propietario = new List<Propietarios>();
+        List<Reporte_propiedad> reporte = new List<Reporte_propiedad>();
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargar_Propiedades();
+            mostrar_Propiedades();
+            cargar_Propietarios();
+            mostrar_Propietarios();
+        }
+        private void cargar_Propiedades()
+        {
+            string fileName = "Propiedades.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                Propiedades propiedades = new Propiedades();
+                propiedades.No_casa = reader.ReadLine();
+                propiedades.Dpi = Convert.ToInt64(reader.ReadLine());
+                propiedades.Cuota_mantenimiento= int.Parse(reader.ReadLine());
+                propiedad.Add(propiedades);
+            }
+            reader.Close();
+        }
+        private void mostrar_Propiedades()
+        {
+            dataGridViewPropiedades.DataSource = null;
+            dataGridViewPropiedades.DataSource = propiedad;
+            dataGridViewPropiedades.Refresh();
+        }
+        private void cargar_Propietarios ()
+        {
+            string fileName = "Propietarios.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                Propietarios propietarios = new Propietarios();
+                propietarios.Dpi = Convert.ToInt64(reader.ReadLine());
+                propietarios.Nombre = reader.ReadLine();
+                propietarios.Apellido = reader.ReadLine();
+                propietario.Add(propietarios);
+            }
+            reader.Close();
+        }
+        private void mostrar_Propietarios()
+        {
+            dataGridViewPropietarios.DataSource = null;
+            dataGridViewPropietarios.DataSource = propiedad;
+            dataGridViewPropietarios.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (reporte.Count == 0)
+            {
+                foreach (Propietarios propietarios in propietario)
+                {
+                    Propiedades propiedades = propiedad.FirstOrDefault(c => c.Dpi == propietarios.Dpi);
+                    Reporte_propiedad reportes = new Reporte_propiedad();
+                    reportes.Nombre = propietarios.Nombre;
+                    reportes.Apellido = propietarios.Apellido;
+                    reportes.No_casa = propiedades.No_casa;
+                    reportes.Cuota_mantenimiento = propiedades.Cuota_mantenimiento;
+                    reporte.Add(reportes);
+
+                }
+            }
+        }
+    }
+}
